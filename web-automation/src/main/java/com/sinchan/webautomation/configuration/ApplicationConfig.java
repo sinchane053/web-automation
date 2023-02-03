2b2b2a804a54676fd630961b308c101b0df4751f
Sinchan Saha <sinchan.saha@cognida.ai>
2023-02-03 14:28:05 +0530

    Working JNDI setup

--- web-automation/src/main/java/com/sinchan/webautomation/configuration/ApplicationConfig.java
+++ web-automation/src/main/java/com/sinchan/webautomation/configuration/ApplicationConfig.java
@@ -1,37 +1,37 @@
-//package com.sinchan.webautomation.configuration;
-//
-//
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
-//
-//import javax.persistence.EntityManagerFactory;
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class ApplicationConfig {
-//
-//
-//    /*@Bean(name = "jndiDataSource")
-//    public DataSource getJndiDataSource(){
-//        JndiDataSourceLookup dsLookUp = new JndiDataSourceLookup();
-//        return dsLookUp.getDataSource("java:comp/env/jdbc/testDb");
-//    }
-//
-//    @Bean(name = "jdbcTemplate")
-//    public JdbcTemplate getJdbcTemplate
-//            (@Qualifier("jndiDataSource") DataSource ds){
-//        return new JdbcTemplate(ds);
-//    }*/
-////    @Bean
-////    @ConfigurationProperties("spring.datasource")
-////    public DataSource dataSource() {
-////        return DataSourceBuilder.create().build();
-////    }
-//
-//
-//}
+package com.sinchan.webautomation.configuration;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+
+
+    @Bean(name = "jndiDataSource")
+    public DataSource getJndiDataSource(){
+        JndiDataSourceLookup dsLookUp = new JndiDataSourceLookup();
+        return dsLookUp.getDataSource("java:comp/env/jdbc/testDb");
+    }
+
+    @Bean(name = "jdbcTemplate")
+    public JdbcTemplate getJdbcTemplate
+            (@Qualifier("jndiDataSource") DataSource ds){
+        return new JdbcTemplate(ds);
+    }
+/*    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }*/
+
+
+}
